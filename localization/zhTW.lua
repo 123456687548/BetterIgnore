d98d29c60a8813318991e48e9ab15f4ab0f46f6e
123456687548 <git@essertim.de>
2024-01-02 19:19:38 +0100

    zhTW localization

---
+++ localization/zhTW.lua
@@ -0,0 +1,15 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("BetterIgnore", "zhTW")
+
+L = L or {}
+L["saveBtn"] = "儲存"
+L["cancelBtn"] = "取消"
+L["reasonPopupText"] = "黑名單原因"
+L["blacklistBtn"] = "黑名單"
+L["pardonBtn"] = "寬恕"
+L["dbgCantCreateKey"] = "無法建立玩家姓名與伺服器組合"
+L["addedToBlacklist"] = "已加入 < %s > 到黑名單"
+L["removedFromBlacklist"] = "從黑名單移除 < %s >"
+L["blockedByTooltip"] = "被 %s 阻擋"
+L["reasonTooltip"] = "原因: %s"
+L["dateTooltip"] = "日期: %s"
+L["isBlacklistedTooltip"] = "玩家已被列入黑名單！"
